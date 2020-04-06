a9d8597fcde6581ee839deac85ce2d4dfdcb6574
Lawrence-Chiappelli <37488155+Lawrence-Chiappelli@users.noreply.github.com>
2020-04-06 14:33:02 -0400

    Working button hover. Both color and text update.
    By clicking the button, you'll notice that the over-extended text bleeds behind the updated button. This is being temporarily kept for future optimization purposes.
    
    This seems to be a common "glitch"- I've seen it occur in several other applications, but I still don't know the technical terminology to describe such a glitch.

--- GUI.py
+++ GUI.py
@@ -8,6 +8,8 @@ resolution = (800, 800)
 caption = "Sudoku Puzzle"
 white = (255, 255, 255)
 red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
 black = (0, 0, 0)
 blurple = (44, 47, 51)
 font_type, font_size = ('freesansbold.ttf', 40)
@@ -26,28 +28,42 @@ class Button:
     def draw(self, surface, outline=None):
         """
         Draws the button on the screen
-        :param surface: the window to draw on
-        :param outline: the button is outlined
+        :param surface: the window/surface to draw on
+        :param outline: outline color, if any
         """
+
         if outline:
-            x = self.x-2
-            y = self.y-2
-            w = self.w+4
-            h = self.h+4
+            x = self.x-4
+            y = self.y-4
+            w = self.w+8
+            h = self.h+8
             pygame.draw.rect(surface, outline, (x, y, w, h), 0)
 
-        if self.text:
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.w, self.h), 0)
+
+        if self.text != '':
             font = pygame.font.SysFont('comicsans', 60)
             text = font.render(self.text, 1, white)
             center = (self.x + (self.w//2 - text.get_width()//2), self.y + (self.h//2 - text.get_height()//2))
             surface.blit(text, center)
 
-    def IsOver(self, position):
+        # -----------------------+
+        pygame.display.update()  #
+        # -----------------------+
+
+    def is_over(self, position):
         if self.x < position[0] < self.x + self.w:
             if self.y < position[1] < self.y + self.h:
-                print(f"Over button")
                 return True
         return False
+
+    def update_color(self, color):
+        self.color = color
+        self.draw(window, black)
+
+    def update_text(self, text):
+        self.text = text
+        self.draw(window, black)
 
 
 def text_objects(text, config):
@@ -69,8 +85,8 @@ window.fill(blurple)
 pygame.display.set_caption(caption)
 display_text("Ultimate Suduoku Puzzles")
 
-button = Button("Click this button", red, resolution[0]//2, resolution[1]//2, 250, 100)
-button.draw(window)
+button = Button("Click me!", red, resolution[0]//2, resolution[1]//2, 250, 100)
+button.draw(window, black)
 
 # ---------------------+
 pygame.display.flip()  #
@@ -79,21 +95,24 @@ pygame.display.flip()  #
 
 running = True
 while running:
+
     for event in pygame.event.get():
 
         mouse_position = pygame.mouse.get_pos()
 
         if event.type == pygame.QUIT:
             running = False
-            pygame.quit()  # TODO: Some people recommend this. Necessary
+            pygame.quit()  # TODO: Some people recommend this. Necessary?
             quit()  # And why this?
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if button.IsOver(mouse_position):
-                print(f"Clicked the button")
+            if button.is_over(mouse_position):
+                button.update_text("You clicked me!")
 
         if event.type == pygame.MOUSEMOTION:
-            if button.IsOver(mouse_position):
-                button.color = white
+            if button.is_over(mouse_position):
+                button.update_color(green)
+                button.update_text("Hovering!")
             else:
-                button.color = red
+                button.update_color(red)
+                button.update_text("Click me!")
